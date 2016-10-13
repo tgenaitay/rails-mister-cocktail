ec0fd1e5b5b641bce5d1db3881d62472e5be7bd3
thibault genaitay <tgenaitay@gmail.com>
2016-10-13 17:56:31 +0200

    doses OK

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,61 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :edit, :update, :destroy]
+  before_action :set_cocktail, only: [:new, :create]
+
+  def index
+    @doses = Dose.all
+  end
+
+  def show
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def edit
+  end
+
+  def create
+    @dose = @cocktail.doses.build(dose_params)
+
+    respond_to do |format|
+      if @dose.save
+        format.html { redirect_to @cocktail, notice: 'Dose was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @dose.update(dose_params)
+        format.html { redirect_to @cocktail, notice: 'Dose was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    respond_to do |format|
+      format.html { redirect_to doses_url, notice: 'Dose was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_dose
+      @dose = Dose.find(params[:id])
+    end
+
+    def set_cocktail
+      @cocktail = Cocktail.find(params[:cocktail_id])
+    end
+
+    def dose_params
+      params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+    end
+end
